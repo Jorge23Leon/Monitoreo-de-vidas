79b96bb25cd3683bedbabc6bc45ac7aa3d8c2fc3
Jorge23Leon <tierrainteligente2@gmail.com>
2026-09-18 12:21:06 -0600

    Limpiar backups y archivos antiguos del proyecto

--- ejecutar_android_fisico.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$IpPc
-)
-$ErrorActionPreference = "Stop"
-$url = "http://${IpPc}:8500/"
-Write-Host "Backend: $url" -ForegroundColor Cyan
-flutter pub get
-flutter run --dart-define=CIAGRO_API_BASE_URL=$url
--- ejecutar_android_local.ps1
+++
@@ -1,3 +0,0 @@
-$ErrorActionPreference = "Stop"
-flutter pub get
-flutter run --dart-define=CIAGRO_API_BASE_URL=http://10.0.2.2:8500/
